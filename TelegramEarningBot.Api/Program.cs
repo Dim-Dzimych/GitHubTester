1cf47f27c042ba70f82b79d037c9ac9caea77ab6
Damir Mansurov <d.mansurov@mandarin.io>
2023-04-23 18:54:47 +0300

    fixed cycles

--- TelegramEarningBot.Api/Program.cs
+++ TelegramEarningBot.Api/Program.cs
@@ -1,10 +1,14 @@
+using System.Text.Json.Serialization;
 using Microsoft.EntityFrameworkCore;
 using TelegramEarningBot.Api;
 using TelegramEarningBot.Api.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllers();
+builder.Services.AddControllers().AddJsonOptions(o =>
+{
+    o.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+});
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<EarningBotContext>(opt => opt.UseSqlite(builder.Configuration.GetConnectionString("Db")));
